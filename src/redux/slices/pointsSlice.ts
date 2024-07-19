ba615377ef7eefdbdb79cf05990566b8e2faf85b
Abhishek Kumar <abhishek.kumar@fewerclicks.in>
2024-07-19 18:14:29 +0530

    dispatching points

--- src/redux/slices/pointsSlice.ts
+++ src/redux/slices/pointsSlice.ts
@@ -2,6 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   points: 0,
+  currPoints: 0,
 };
 
 const pointsSlice = createSlice({
@@ -11,8 +12,11 @@ const pointsSlice = createSlice({
     setPoints: (state) => {
       state.points += 1;
     },
+    getCurrPoints: (state, action)=>{
+      state.currPoints = action.payload
+    }
   },
 });
 
-export const { setPoints } = pointsSlice.actions;
+export const { setPoints, getCurrPoints } = pointsSlice.actions;
 export default pointsSlice.reducer;
